9967e7da00520a643f0b2695c985132c6fd8dd8b
kusa <kusafong1137@gmail.com>
2025-11-20 15:28:40 +0800

    [add] 新增共用列表分頁_Pagination.cshtml

---
+++ Areas/Models/PaginationInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace ADAMS.Areas.Models
+{
+    public class PaginationInfo
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalCount { get; set; }
+        public int PageSize { get; set; }
+        public int[] PageSizeOptions { get; set; } = new[] { 5, 10, 15, 20 };
+
+        /// <summary>
+        /// 存放所有搜索條件
+        /// Key: 參數名稱 (如 "statusFilter", "keyword", "startDate")
+        /// Value: 參數值 (支援任何類型)
+        /// </summary>
+        public Dictionary<string, object> SearchFilters { get; set; } = new();
+    }
+}
