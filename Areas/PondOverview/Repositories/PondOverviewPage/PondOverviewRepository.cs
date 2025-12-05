cfb9361ad4381f842ff9443fbe1dae971f68a35f
kusa <kusafong1137@gmail.com>
2025-12-05 16:35:40 +0800

    [update] 養殖池總覽詳細資料(首頁PondOverviewPage)

--- Areas/PondOverview/Repositories/PondOverviewPage/PondOverviewRepository.cs
+++ Areas/PondOverview/Repositories/PondOverviewPage/PondOverviewRepository.cs
@@ -94,7 +94,110 @@ namespace ADAMS.Areas.PondOverview.Repositories.PondOverviewPage
 
             if (pond == null) return null;
 
-            // ★ 下面多數欄位先留預留值，等你接實際表格（放養紀錄 / 採樣紀錄 / 收成紀錄等）後再改查詢
+            // 找最新一筆尚未刪除的放苗紀錄
+            var fry = await _context.FryRecord
+                .Include(fr => fr.Fry)
+                    .ThenInclude(f => f.FishVariety)
+                .Where(fr => fr.PondSN == pondSN && !fr.IsDeleted)
+                .OrderByDescending(fr => fr.FarmingDate)
+                .FirstOrDefaultAsync();
+
+            string managerName = "";
+            string farmingCode = "";
+            string fishVarietyName = "";
+            string plOrGram = "";
+            int stockingQty = 0;
+            decimal density = 0m;
+            DateTime? stockingDate = null;
+
+            decimal currentABW = 0m;
+            int doc = 0;
+            DateTime? lastSampleOrHarvestDate = null;
+
+            decimal totalHarvestWeight = 0m;
+            int totalHarvestPCS = 0;
+            int harvestTimes = 0;
+            decimal survivalRate = 0m;
+
+            if (fry != null)
+            {
+                managerName = fry.ManageAccount ?? "";
+                farmingCode = fry.FarmingCode;
+                fishVarietyName = fry.Fry?.FishVariety?.FVName ?? "";
+                stockingQty = fry.FarmingPCS;
+                stockingDate = fry.FarmingDate;
+
+                // 密度：尾數 / 面積
+                if (pond.PondArea > 0)
+                    density = fry.FarmingDensity > 0
+                        ? fry.FarmingDensity
+                        : Math.Round((decimal)fry.FarmingPCS / pond.PondArea, 2, MidpointRounding.AwayFromZero);
+
+                plOrGram = fry.FryAge.ToString("0.##");
+            }
+
+            if (!string.IsNullOrEmpty(farmingCode))
+            {
+                //找最近的採樣與收穫，取日期較新的那一筆當「最近收成/採樣日」+ ABW/DOC
+                var lastSampling = await _context.SamplingRecord
+                    .Where(s => s.PondSN == pondSN &&
+                                s.FarmingCode == farmingCode &&
+                                !s.IsDeleted)
+                    .OrderByDescending(s => s.SamplingDate)
+                    .FirstOrDefaultAsync();
+
+                var lastHarvest = await _context.HarvestRecord
+                    .Where(h => h.PondSN == pondSN &&
+                                h.FarmingCode == farmingCode &&
+                                !h.IsDeleted)
+                    .OrderByDescending(h => h.HarvestDate)
+                    .FirstOrDefaultAsync();
+
+                if (lastSampling != null || lastHarvest != null)
+                {
+                    // 採樣比較新 or 只有採樣
+                    if (lastHarvest == null ||
+                        (lastSampling != null && lastSampling.SamplingDate >= lastHarvest.HarvestDate))
+                    {
+                        lastSampleOrHarvestDate = lastSampling!.SamplingDate;
+                        currentABW = lastSampling.ABW;
+                        doc = lastSampling.DOC;
+                    }
+                    // 收穫比較新
+                    else
+                    {
+                        lastSampleOrHarvestDate = lastHarvest!.HarvestDate;
+                        currentABW = lastHarvest.ABW;
+                        doc = lastHarvest.DOC;
+                    }
+                }
+
+                //累計收成重量 / 尾數 / 次數
+                var harvestQuery = _context.HarvestRecord
+                    .Where(h => h.PondSN == pondSN &&
+                                h.FarmingCode == farmingCode &&
+                                !h.IsDeleted);
+
+                totalHarvestWeight = await harvestQuery.SumAsync(h => (decimal?)h.HarvestWeight) ?? 0m;
+                totalHarvestPCS = await harvestQuery.SumAsync(h => (int?)h.HarvestPCS) ?? 0;
+                harvestTimes = await harvestQuery.CountAsync();
+
+                //存活率 = 累計收穫尾數 / 放養尾數
+                if (fry != null && fry.FarmingPCS > 0)
+                {
+                    survivalRate = Math.Round(
+                        (decimal)totalHarvestPCS / fry.FarmingPCS * 100m,
+                        2,
+                        MidpointRounding.AwayFromZero);
+                }
+            }
+
+            // 如果沒有任何採樣/收穫紀錄，就用「今天」算目前 DOC
+            if (doc == 0 && fry != null)
+            {
+                doc = (int)(DateTime.Today.Date - fry.FarmingDate.Date).TotalDays;
+            }
+
             return new PondDetailViewModel
             {
                 PondSN = pond.PondSN,
@@ -103,24 +206,28 @@ namespace ADAMS.Areas.PondOverview.Repositories.PondOverviewPage
                 AreaName = pond.Area?.AreaName ?? "",
                 TenantName = pond.Tenant?.TenantName ?? "",
 
-                ManagerName = "",                 // TODO: 管理員
-                StockingCode = "",                // TODO: 放養碼
-                FishVarietyName = "",             // TODO: 放養種類
-                PLorGramUnit = "",                // TODO: PL 或 克/尾
-                StockingQty = 0,                  // TODO
-                Density = 0m,                     // TODO
+                // 來自 FryRecord
+                ManagerName = managerName,
+                StockingCode = farmingCode,
+                FishVarietyName = fishVarietyName,
+                PLorGramUnit = plOrGram,
+                StockingQty = stockingQty,
+                Density = density,
+                StockingDate = stockingDate,
 
-                StockingDate = null,              // TODO
-                LastSampleOrHarvestDate = null,   // TODO
-                CurrentABW = 0m,                  // TODO
-                DOC = 0,                          // TODO
+                // 來自 Sampling / Harvest
+                LastSampleOrHarvestDate = lastSampleOrHarvestDate,
+                CurrentABW = currentABW,
+                DOC = doc,
 
-                AccumulatedHarvestWeight = 0m,    // TODO
-                HarvestTimes = 0,                 // TODO
-                SurvivalRate = 0m,                // TODO
+                // 來自 HarvestRecord + FryRecord
+                AccumulatedHarvestWeight = totalHarvestWeight,
+                HarvestTimes = harvestTimes,
+                SurvivalRate = survivalRate,
 
                 Remark = pond.Remark
             };
         }
+
     }
 }
